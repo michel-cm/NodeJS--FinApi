fdebad530b79756b5201f13b7d4a0c6086df74f3
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-10-24 09:20:35 -0300

    Middleware

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).send({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -29,15 +44,8 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).send({ error: "Customer not found" });
-  }
-
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
